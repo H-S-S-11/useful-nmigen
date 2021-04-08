719da2fce08ff032b5539ea6b1f64d0fed4d3546
H-S-S-11 <harry@snell.org.uk>
2021-04-08 21:01:24 +0100

    fix clocking for ml505

--- utility/ml505_luna_clocking.py
+++ utility/ml505_luna_clocking.py
@@ -17,13 +17,15 @@ class ML505LunaClockDomains(Elaboratable):
 
         clk100 = platform.request("clk100")
 
-        # Instantiate USB Full-speed PLL
+        # Instantiate USB Full-speed PLL and required BUFGs
         clk12       = Signal()
         clk48       = Signal()
+        clk12_buf   = Signal()
+        clk48_buf   = Signal()
         usb2_lock   = Signal()
         usb2_fb     = Signal()
         m.submodules.usb_pll = Instance("PLL_ADV",
-            p_BANDWIDTH             = "OPTIMISED",
+            p_BANDWIDTH             = "OPTIMIZED",
             p_COMPENSATION          = "SYSTEM_SYNCHRONOUS",
             p_DIVCLK_DIVIDE         = 5,
             p_CLKFBOUT_MULT         = 24,
@@ -34,6 +36,7 @@ class ML505LunaClockDomains(Elaboratable):
             p_CLKOUT1_PHASE         = 0.00,
             p_CLKOUT1_DUTY_CYCLE    = 0.500,
             p_CLKIN1_PERIOD         = 10.000,
+            i_CLKINSEL              = Const(1),
             i_CLKFBIN               = usb2_fb,
             o_CLKFBOUT              = usb2_fb,
             i_CLKIN1                = clk100,
@@ -41,11 +44,19 @@ class ML505LunaClockDomains(Elaboratable):
             o_CLKOUT1               = clk48,
             o_LOCKED                = usb2_lock,
         )
+        m.submodules.clk12_bufg = Instance("BUFG",
+            i_I = clk12,
+            o_O = clk12_buf,
+        )
+        m.submodules.clk48_bufg = Instance("BUFG",
+            i_I = clk48,
+            o_O = clk48_buf,
+        )
 
         # Connect clocks to domains
         m.d.comb += [
-            ClockSignal("usb")      .eq(clk12),
-            ClockSignal("usb_io")   .eq(clk48),
+            ClockSignal("usb")      .eq(clk12_buf),
+            ClockSignal("usb_io")   .eq(clk48_buf),
             ResetSignal("usb")      .eq(~usb2_lock),
             ResetSignal("usb_io")   .eq(~usb2_lock),
         ]
