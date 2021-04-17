595eb8fc61d9105d7e84dd3a45dcf564d52db8bd
H-S-S-11 <harry@snell.org.uk>
2021-04-17 21:53:21 +0100

    finish testing rx

--- utility/uart.py
+++ utility/uart.py
@@ -6,8 +6,7 @@ from nmigen.lib.cdc import *
 class UART_RX(Elaboratable):
     def __init__(self, baud_rate=9600, fclk=None):
         self.rx = Signal()
-        self.tx = Signal()
-
+        
         self.error  = Signal()
         self.data   = Signal(8)
         self.valid  = Signal()
@@ -56,9 +55,10 @@ class UART_RX(Elaboratable):
                     baud_divide.eq(0),
                     oversample_counter.eq(0),
                 ]
-            # Early changes
+            # changes
             with m.Else():
-                with m.If(oversample_counter[2]):
+                with m.If(~oversample_counter[0:2].any() | 
+                    (oversample_counter[0] & oversample_counter[2])):
                     m.d.sync += [
                         baud_divide.eq(0),
                         oversample_counter.eq(0),
@@ -77,10 +77,16 @@ class UART_RX(Elaboratable):
             m.d.sync += voting.eq(Cat(voting[1], rx_sync))
         m.d.comb += vote.eq( voting.all() | (rx_sync & voting[0]) | (rx_sync & voting[1]))
         
-        with m.If(~(bit_count==10) & sample_strobe & (oversample_counter==3)):
-            m.d.sync += self.data.eq(Cat(self.data[1:8], vote))
+        # Shift into data output reg, check stop bit
+        with m.If(sample_strobe & oversample_counter[2]):
+            with m.If(bit_count==10):
+                m.d.sync += [
+                    self.valid.eq(vote),
+                    self.error.eq(~vote),
+                ]
+            with m.Else():
+                m.d.sync += self.data.eq(Cat(self.data[1:8], vote))
 
-        m.d.comb += self.tx.eq(rx_sync)
 
         return m
 
@@ -93,27 +99,77 @@ if __name__=="__main__":
         while True:
             yield
 
-    def tx_byte(byte):
+    def tx_byte(byte, jitter=400):
         bits = bin(byte)[2:]
         while len(bits) < 8:
             bits = "0" + bits
         yield u.rx.eq(0)    # Start bit
-        yield Delay(interval=(random.randrange(8500, 8800)/1e9))
+        yield Delay(interval=(random.randrange(8680-jitter, 8680+jitter)/1e9))
         for n in range(0, 8):
             yield u.rx.eq(int(bits[7-n]))
-            yield Delay(interval=(random.randrange(8500, 8800)/1e9))
+            yield Delay(interval=(random.randrange(8680-jitter, 8680+jitter)/1e9))
         yield u.rx.eq(1)    # Stop bit
-        yield Delay(interval=(random.randrange(8500, 8800)/1e9))
+        yield Delay(interval=(random.randrange(8680-jitter, 8680+jitter)/1e9))
+
+    def noisy_byte(byte):
+        bits = bin(byte)[2:]
+        while len(bits) < 8:
+            bits = "0" + bits
+        yield u.rx.eq(0)    # Start bit
+        yield Delay(interval=8.68e-6)
+
+        yield u.rx.eq(int(bits[7]))
+        yield Delay(interval=2.8e-6)
+        yield u.rx.eq(~u.rx)
+        yield Delay(interval=0.4e-6)
+        yield u.rx.eq(~u.rx)
+        yield Delay(interval=3e-6)
+
+        yield u.rx.eq(int(bits[6]))
+        yield Delay(interval=4.2e-6)
+        yield u.rx.eq(~u.rx)
+        yield Delay(interval=0.4e-6)
+        yield u.rx.eq(~u.rx)
+        yield Delay(interval=3e-6)
+
+        yield u.rx.eq(int(bits[5]))
+        yield Delay(interval=5.6e-6)
+        yield u.rx.eq(~u.rx)
+        yield Delay(interval=0.4e-6)
+        yield u.rx.eq(~u.rx)
+        yield Delay(interval=2.8e-6)
+
+        for n in range(3, 8):
+            yield u.rx.eq(int(bits[7-n]))
+            yield Delay(interval=8.68e-6)
+        yield u.rx.eq(1)    # Stop bit
+        yield Delay(interval=8.68e-6)
 
     import random
     def tb():
         yield u.rx.eq(1)
-        yield Delay(interval=(random.randrange(8000, 10000)/1e9))
-        yield from tx_byte(0x56)
+        yield Delay(interval=(random.randrange(7000, 11000)/1e9))
+        yield from tx_byte(0x56, jitter=1000)
+        yield Delay(interval=4e-5)
+        yield from noisy_byte(0xa5)
+        for n in range(0, 10):
+            yield Delay(interval=4e-5)
+            yield from tx_byte(0x56, jitter=1000)
+
+    def check_output():
+        error = valid = 0
+        while not (error or valid):
+            error = yield u.error
+            valid = yield u.valid
+            yield
+        yield
+        # assert not (yield u.error)
+        # assert (yield u.data)==0x56
 
     sim.add_sync_process(clock)
     sim.add_process(tb)
+    sim.add_sync_process(check_output)
 
     with sim.write_vcd("UART_waves.vcd"):
-        sim.run_until(1e-3)
+        sim.run_until(1.6e-3)
         
